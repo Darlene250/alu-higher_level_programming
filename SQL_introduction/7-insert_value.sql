3ddc5ff6d72aef278e926acbb1d1a16f85453da5
Akinloye3264 <e.akinloye@alustudent.com>
2025-03-13 07:22:18 -0700

    ins val

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- insert
-INSERT INTO first_table(id, name)
-VALUE(89, "Holberton School");
+-- Inserts a new row into the table first_table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
