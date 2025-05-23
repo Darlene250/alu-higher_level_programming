888d0e123ac65c3ed6ee869f3fc4100fea111d64
Akinloye3264 <e.akinloye@alustudent.com>
2025-05-23 05:28:08 -0700

    multi

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-let string1="C is fun";
-let string2="Python is cool";
-let string3="JavaScript is amazing";
-
-console.log(string1);
-console.log(string2);
-console.log(string3);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
