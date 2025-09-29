e23043764fb2401199ba333ec9a5fc6c7715da6d
Darlene250 <d.ayinkamiy@alustudent.com>
2025-09-29 20:10:43 +0200

    Update 10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,13 +1,20 @@
 #!/usr/bin/node
 
+const args = process.argv;
+const numA = Number(args[2]);
 
 function factorial (n) {
-	  if (isNaN(n)) return 1; // Factorial of NaN is 1
-	  if (n <= 1) return 1; // Base case: factorial of 0 or 1 is 1
-	  return n * factorial(n - 1);
-} 
+  if (n === 0) {
+    return 1;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
 
-// Get the first argument, cast it to integer
- const arg = parseInt(process.argv[2]);
-
- console.log(factorial(arg));
+if (numA) {
+  console.log(factorial(numA));
+} else {
+  console.log(1);
+}
