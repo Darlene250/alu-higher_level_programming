c2ca681ef1150493e86f28bdd16b295535dc6750
Darlene250 <d.ayinkamiy@alustudent.com>
2025-09-29 20:06:24 +0200

    Update 3-value_argument.js

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
-	  console.log('No argument');
+
+const arg = process.argv;
+
+if (arg[2] != null) {
+  console.log(arg[2]);
 } else {
-	  console.log(process.argv[2]);
+  console.log('No argument');
 }
