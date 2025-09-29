dcb19d5c011290793b2eacf596067b1a363a1ea1
Darlene250 <d.ayinkamiy@alustudent.com>
2025-09-29 20:11:07 +0200

    Update 11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,21 @@
 #!/usr/bin/node
-const args = process.argv.slice(2).map(Number); // Convert all arguments to integers
 
-if (args.length <= 1) {
-	  console.log(0); // Print 0 if no arguments or only one argument
+const args = process.argv;
+const numA = Number(args[2]);
+const numB = Number(args[3]);
+let firstBig = numA;
+let secondBig = numB;
+
+if (args.length <= 3) {
+  console.log(0);
 } else {
-	  const secondBiggest = args.sort((a, b) => b - a)[1];
-	  console.log(secondBiggest);
+  for (let a = 3; a < args.length; a++) {
+    if (Number(args[a]) > firstBig) {
+      secondBig = firstBig;
+      firstBig = Number(args[a]);
+    } else if (Number(args[a]) > secondBig && Number(args[a]) < firstBig) {
+      secondBig = Number(args[a]);
+    }
+  }
+  console.log(secondBig);
 }
